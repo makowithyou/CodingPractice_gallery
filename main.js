9bdc70394c76a3b57fec117b298ea67cba01204e
makowithyou <mako.withyou@gmail.com>
2025-02-14 13:56:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,10 @@ const HamburgerButton = document.getElementById("HamburgerButton");
 const HamburgerMenu = document.getElementById("HamburgerMenu");
 HamburgerButton.addEventListener(
     "click", (e) => {
-        Spans = e.currentTarget.children;
-        if (HamburgerMenu.classList.length == 0) {
-            for (span of Spans) {
-                span.classList.add("open");
-            }
-            HamburgerMenu.classList.add("open");
-        } else {
-            for (span of Spans) {
-                span.classList.toggle("open");
-                span.classList.toggle("close");
-            }
-            HamburgerMenu.classList.toggle("open");
-            HamburgerMenu.classList.toggle("close");
+    Spans = e.currentTarget.children;
+        for (span of Spans) {
+            span.classList.toggle("open");
         }
+        HamburgerMenu.classList.toggle("open");
     }
 )
