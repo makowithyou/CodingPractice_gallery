3cc62a98f65b9d888b2f55838839d70375cb0851
makowithyou <mako.withyou@gmail.com>
2025-02-17 14:20:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-// スクロールしてヘッダーを表示
+// スクロールイベント
 const Header = document.getElementById("Header");
+const MainVisual = document.getElementById("MainVisual");
+const Title = document.getElementById("Title");
+const WH = window.innerHeight;
 addEventListener(
     "scroll", () => {
+
+        // スクロールしてヘッダーを表示
         if (window.scrollY > 520) {
             Header.classList.add("show");
         } else {
             Header.classList.remove("show");
+        }
+
+        // Titleが画面下にきたら下からふわっと表示
+        const TY = Title.getBoundingClientRect().y;
+        if (TY < WH) {
+            Title.classList.add("show");
         }
     }
 )
